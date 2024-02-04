c45e3f0d0e3580a48ae0f13bf4aec8609bcf942e
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-02-03 21:21:54 -0500

    Create NeuralNetwork class which is made up of multiple layers

--- src/main/java/com/nn/NeuralNetwork.java
+++ src/main/java/com/nn/NeuralNetwork.java
@@ -1,11 +1,12 @@
-package com.wavesneuralnetwork;
+package com.nn;
 
 import java.util.HashMap;
 
 public class NeuralNetwork {
     private final double learningRate;
-    private int[] sizes;
-    private Layer[] layers;
+    private final int[] sizes;
+    private final Layer[] layers;
+    private final String activationFunction;
     private static final HashMap<String, ActivationFunction> activationFunctions = new HashMap<>() {{
         put("sigmoid", ActivationFunction.SIGMOID);
         put("tanh", ActivationFunction.TANH);
@@ -20,12 +21,27 @@ public class NeuralNetwork {
         if (!activationFunctions.containsKey(activationFunction))
             throw new IllegalArgumentException("Activation function not found");
 
+        this.activationFunction = activationFunction;
+
         for (int i = 0; i < sizes.length; i++) {
             int nextSize = i + 1 < sizes.length ? sizes[i + 1] : 0;
             layers[i] = new Layer(sizes[i], nextSize, activationFunctions.get(activationFunction));
         }
     }
 
+    public NeuralNetwork(double learningRate, int... sizes) {
+        this(learningRate, "sigmoid", sizes);
+    }
 
+    public NeuralNetwork clone() {
+        NeuralNetwork neuralNetwork = new NeuralNetwork(learningRate, activationFunction, sizes);
+        for (int i = 0; i < layers.length; i++)
+            neuralNetwork.layers[i] = layers[i].clone();
+        return neuralNetwork;
+    }
+
+    public void mutate() {
+        for (Layer layer : layers) layer.mutate(learningRate);
+    }
 
 }
