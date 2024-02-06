c96b188d4a02dc3bc7cfcc35df85000d1ef01dd7
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-02-05 21:53:37 -0500

    Adapt to creation of Matrix Class and add JavaDocs

--- src/main/java/com/nn/ActivationFunction.java
+++ src/main/java/com/nn/ActivationFunction.java
@@ -1,42 +1,82 @@
 package com.nn;
 
+/**
+ * Enum for activation functions used in the neural network.
+ * <p>
+ *     The enum contains the following activation functions:
+ *     <ul>
+ *         <li>Sigmoid</li>
+ *         <li>Tanh</li>
+ *         <li>ReLU</li>
+ *     </ul>
+ * </p>
+ */
 enum ActivationFunction {
+    /**
+     * Sigmoid activation function.
+     * <br>
+     * f(x) = 1 / (1 + e^(-x))
+     * <br>
+     * f`(x) = f(x) * (1 - f(x))
+     */
     SIGMOID {
         @Override
-        public double function(double x) {
-            return 1 / (1 + Math.exp(-x));
+        public Matrix function(Matrix m) {
+            return m.map(x -> 1 / (1 + Math.exp(-((double) x))));
         }
 
         @Override
-        public double derivative(double x) {
-            return function(x) * (1 - function(x));
+        public Matrix derivative(Matrix m) {
+            return m.map(x -> (double) x * (1 - (double) x));
         }
     },
+    /**
+     * Tanh activation function.
+     * <br>
+     * f(x) = tanh(x)
+     * <br>
+     * f`(x) = 1 - f(x)^2
+     */
     TANH {
         @Override
-        public double function(double x) {
-            return Math.tanh(x);
+        public Matrix function(Matrix m) {
+            return m.map(x -> Math.tanh((double) x));
         }
 
         @Override
-        public double derivative(double x) {
-            return 1 - Math.pow(function(x), 2);
+        public Matrix derivative(Matrix m) {
+            return m.map(x -> 1 - Math.pow((double) x, 2));
         }
     },
+    /**
+     * ReLU activation function.
+     * <br>
+     * f(x) = max(0, x)
+     * <br>
+     * f`(x) = 1 if x > 0, 0 otherwise
+     */
     RELU {
         @Override
-        public double function(double x) {
-            return Math.max(0, x);
+        public Matrix function(Matrix m) {
+            return m.map(x -> Math.max(0, (double) x));
         }
 
         @Override
-        public double derivative(double x) {
-            return x > 0 ? 1 : 0;
+        public Matrix derivative(Matrix m) {
+            return m.map(x -> (double) x > 0 ? 1 : 0);
         }
     };
 
-    public abstract double function(double x);
+    /**
+     * @param m matrix to apply the function to
+     * @return a new matrix that is the result of applying the function to each element of the input matrix
+     */
+    public abstract Matrix function(Matrix m);
 
-    public abstract double derivative(double x);
+    /**
+     * @param m matrix to apply the derivative function to
+     * @return a new matrix that is the result of applying the derivative of the function to each element of the input matrix
+     */
+    public abstract Matrix derivative(Matrix m);
 
 }
