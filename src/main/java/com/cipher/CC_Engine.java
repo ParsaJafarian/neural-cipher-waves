f09a1a1c84724e08763a33a9b94231cab47d181e
ParsaMorsh <123512433+ParsaMorsh@users.noreply.github.com>
2024-03-01 08:14:13 -0500

    Full Classic Caesar Engine Complete

--- src/main/java/com/cipher/CC_Engine.java
+++ src/main/java/com/cipher/CC_Engine.java
@@ -1,7 +1,6 @@
 package com.cipher;
 
-import java.util.Arrays;
-import org.apache.commons.math3.stat.inference.ChiSquareTest;
+
 
 public class CC_Engine {
     public static void main(String[] args) {
@@ -10,26 +9,8 @@ public class CC_Engine {
         String deText = CC_decrypt(enText, 3);
         System.out.println(deText);
     }
-
     static String CC_decrypt(String encrypted, int key){
         return CC_encrypt(encrypted, 26 - (key % 26));
-    }
-    static int CC_findkey(String encryptee){
-        double[] englishLettersProbabilities = {0.073, 0.009, 0.030, 0.044, 0.130, 0.028, 0.016, 0.035, 0.074,
-                0.002, 0.003, 0.035, 0.025, 0.078, 0.074, 0.027, 0.003,
-                0.077, 0.063, 0.093, 0.027, 0.013, 0.016, 0.005, 0.019, 0.001};
-        double[] expectedLettersFrequencies = Arrays.stream(englishLettersProbabilities)
-                .map(probability -> probability * encryptee.length())
-                .toArray();
-        double[] chiSquares = new double[26];
-
-        for (int key = 0; key < chiSquares.length; key++) {
-            String decipheredMessage = CC_decrypt(encryptee, key);
-            long[] lettersFrequencies = observedLettersFrequencies(decipheredMessage);
-            double chiSquare = new ChiSquareTest().chiSquare(expectedLettersFrequencies, lettersFrequencies);
-            chiSquares[key] = chiSquare;
-        }
-        return chiSquares;
     }
     static String CC_encrypt(String encryptee, int key){
         StringBuilder encrypted = new StringBuilder();
