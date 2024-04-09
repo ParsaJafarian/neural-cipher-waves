befe41ad03774c02291facbe9e73d4cfeea30898
David Tang <2278304@edu.vaniercollege.qc.ca>
2024-04-09 14:01:56 -0400

    Finished functionality for pendulum and modified some values in the sliders for optimal use.

--- src/PendulumController.java
+++ src/PendulumController.java
@@ -16,6 +16,9 @@ import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Shape;
 
 /**
@@ -44,6 +47,17 @@ public class PendulumController implements Initializable {
     private Button Shape1;
     @FXML
     private Button button2;
+    @FXML
+    private Slider lengthSlider;
+    @FXML
+    private Slider angleSlider;
+
+    double angleVal;
+    double length;
+    @FXML
+    private Line lineReference;
+    @FXML
+    private AnchorPane back;
 
     /**
      * Initializes the controller class.
@@ -52,34 +66,50 @@ public class PendulumController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         ropeSet(object);
-        System.out.println(path.toString());
+        angleVal = angleSlider.getValue();
+        length = lengthSlider.getValue();
         PathTransition pathTransition = new PathTransition(new Duration(800), path, object);
         pathTransition.setAutoReverse(true);
         pathTransition.setCycleCount(Animation.INDEFINITE);
-        pathTransition.play();
+        arcPathCreation(lengthSlider.getValue(), angleSlider.getValue(), pathTransition);
+        
+
+        lengthSlider.valueProperty().addListener((observable, oldvalue, newvalue) -> {
+            length = (double) newvalue;
+            arcPathCreation(length, angleVal,pathTransition);
+        });
+        angleSlider.valueProperty().addListener((observable, oldvalue, newvalue) -> {
+            angleVal = (double) newvalue;
+            arcPathCreation(length, angleVal,pathTransition);
+        });
 
         Shape1.setOnAction(e -> {
-            System.out.println(pathTransition.durationProperty());
-            pathTransition.getNode().setVisible(false);
+            if (!pathTransition.getNode().equals(object2)) {
+                pathTransition.getNode().setVisible(false);
+            }
             ropeSet(object2);
             pathTransition.setNode(object2);
             pathTransition.stop();
             pathTransition.play();
         });
-        
-        
-    /*
-        rope2.startXProperty().bind(object.translateXProperty().add(object.centerXProperty()));
-        rope2.startYProperty().bind(object.translateYProperty().add(object.centerYProperty()));
-        rope2.endXProperty().bind(object2.translateXProperty().add(object.getCenterX()));
-        rope2.endYProperty().bind(object2.translateYProperty().add(object.getCenterY()));
-        PathTransition pathTransition2 = new PathTransition(new Duration(800), path2, object2);
-        pathTransition2.setAutoReverse(true);
-        pathTransition2.setCycleCount(Animation.INDEFINITE);
-        pathTransition2.play();
-     */
-}
-    
+
+    }
+
+    public void arcPathCreation(double length, double angle, PathTransition pathTransition) {
+        double degrees = angle * (Math.PI / 180);
+        double yDifference = length - (length * Math.cos(degrees));
+        double xDifference = length* Math.sin(degrees);
+       
+        path.setLayoutX(0);
+        path.setLayoutY(0);
+        path.setCenterX(lineReference.getLayoutX());
+        path.setCenterY(length);
+        path.setRadiusX(xDifference);
+        path.setRadiusY(yDifference);
+        pathTransition.stop();
+        pathTransition.play();
+    }
+
     public void ropeSet(Circle obj) {
         rope.startXProperty().bind(pivot.centerXProperty());
         rope.startYProperty().bind(pivot.centerYProperty());
