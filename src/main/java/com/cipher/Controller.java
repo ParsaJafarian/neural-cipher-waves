7cc436dcbb5a3b02599721ee919a733144fdf0aa
2264859 <2264859@edu.vaniercollege.qc.ca>
2024-04-19 08:42:16 -0400

    Animation done!!!!!!!!!!!

--- src/main/java/com/cipher/Controller.java
+++ src/main/java/com/cipher/Controller.java
@@ -1,12 +1,19 @@
 package com.cipher;
 
+import javafx.animation.RotateTransition;
+import javafx.animation.Transition;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
+import javafx.scene.transform.Rotate;
 
 
 public class Controller {
+    public double startI;
+    public double startII;
+    public double startO;
+    public double startOO;
 
     public Circle innerC;
     public Circle outerC;
@@ -21,25 +28,33 @@ public class Controller {
         double mouseDeltaX = e.getX();
         double mouseDeltaY = e.getY();
         double radAngle = Math.atan2(mouseDeltaY, mouseDeltaX);
-        innerC.setRotate(Math.toDegrees(radAngle));
+        startI = Math.toDegrees(radAngle);
+        startII = inner.getRotate();
     }
     @FXML
     public void onMousePressedO(MouseEvent e){
-        System.out.println(e.getX());
+        double mouseDeltaX = e.getX();
+        double mouseDeltaY = e.getY();
+        double radAngle = Math.atan2(mouseDeltaY, mouseDeltaX);
+        startO = Math.toDegrees(radAngle);
+        startOO = outer.getRotate();
     }
     @FXML
     public void onDraggedI(MouseEvent t){
         double mouseDeltaX = t.getX();
         double mouseDeltaY = t.getY();
         double radAngle = Math.atan2(mouseDeltaY, mouseDeltaX);
-        System.out.println(radAngle);
-        System.out.println(innerC.getRotate());
-        double deltaAngle = Math.toDegrees(radAngle)-innerC.getRotate();
-        //inner.setRotate(deltaAngle);
-        //System.out.println(deltaAngle);
+        double deltaAngle = Math.toDegrees(radAngle)-startI;
+        System.out.println(deltaAngle);
+        inner.setRotate(startII+deltaAngle);
     }
     @FXML
     public void onDraggedO(MouseEvent t){
-        System.out.println("Mouse Dragged on the Outer");
+        double mouseDeltaX = t.getX();
+        double mouseDeltaY = t.getY();
+        double radAngle = Math.atan2(mouseDeltaY, mouseDeltaX);
+        double deltaAngle = Math.toDegrees(radAngle)-startO;
+        System.out.println(deltaAngle);
+        outer.setRotate(startOO+deltaAngle);
     }
 }
